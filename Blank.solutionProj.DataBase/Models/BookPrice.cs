0c761ba5a95973de953c9cc207c8aceaa38fe87b
intikhab-h-bhat <intikhabh@gmail.com>
2024-09-01 20:08:09 +0530

    added currency and book frice classes in model

---
+++ Blank.solutionProj.DataBase/Models/BookPrice.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blank.solutionProj.DataBase.Models
+{
+    internal class BookPrice
+    {
+        public int Id { get; set; }
+        public int Price { get; set; }
+        
+
+    }
+}
