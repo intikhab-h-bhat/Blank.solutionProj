ab28ca8ebddcd964c01b27b1f0f3d1198f52f88b
intikhab-h-bhat <intikhabh@gmail.com>
2024-09-02 12:53:49 +0530

    usid the async  ,task and  await in  controller actions

--- Blank.SolutionProj.WebApi/Controllers/CurrencyController.cs
+++ Blank.SolutionProj.WebApi/Controllers/CurrencyController.cs
@@ -1,6 +1,7 @@
 ﻿using Blank.solutionProj.DataBase.Data;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Blank.SolutionProj.WebApi.Controllers
 {
@@ -13,17 +14,36 @@ namespace Blank.SolutionProj.WebApi.Controllers
         {
 
             _appDbContext = appDbContext;
-            
+
         }
 
         [HttpGet]
-        public IActionResult GetAllCurrencys() {
-        
-            
+        public async Task<IActionResult> GetAllCurrencys() {
+
+
             //var result= _appDbContext.Currencys.ToList();
-            var result = (from currency in _appDbContext.Currencys select currency).ToList();
+            // var result = (from currency in _appDbContext.Currencys select currency).ToList();
+
+            //var result= await _appDbContext.Currencys.ToListAsync();
+            var result = await (from currency in _appDbContext.Currencys select currency).ToListAsync();
+
+
             return Ok(result);
-        
+
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCurrencyByIdAsync([FromRoute] int id)
+        {
+
+
+
+            var result= await _appDbContext.Currencys.FindAsync(id);
+          // var result = await (from currency in _appDbContext.Currencys where currency.Id==id  select currency).ToListAsync();
+
+
+            return Ok(result);
+
         }
 
     }
